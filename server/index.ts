e8ccd49727f3647bf1a65cbee778cdb5f043a9d9
Md. Sabbir Hossain <shr472978@gmail.com>
2024-02-06 20:28:47 +0600

    sqlite correction

--- server/index.ts
+++ server/index.ts
@@ -1,50 +1,61 @@
-import express, { Request, Response } from "express";
-const PORT = process.env.PORT || 5000;
-const cors = require("cors");
-// let db = require("./db");
-
+import express, { Request, Response } from 'express';
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+// const PORT = process.env.PORT || 5000;
+const DBSOURCE = './dua_main.sqlite';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// open the database
+let db = new sqlite3.Database(DBSOURCE, sqlite3.OPEN_READWRITE, (err: any) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
 // get post routes
 
-// app.get("/duas", (req: Request, res: Response) => {
-//     let sql = "select * from dua";
-//     let params = [];
-//     db.all(sql, params, (err: any, rows: any) => {
-//       if (err) {
-//         res.status(400).json({ error: err.message });
-//         return;
-//       }
-//       res.json({
-//         message: "success",
-//         data: rows,
-//       });
-//     });
-// });
-
-// app.get("/duas/duacategory", (req: Request, res: Response) => {
-//   let sql = "select * from category";
-//   let params = [];
-//   db.all(sql, params, (err: any, row: any) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "success",
-//       data: row,
-//     });
-//   });
-// });
-
-// root routes
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello Next" });
+app.get('/duas', (req: Request, res: Response) => {
+  let sql = 'select * from dua';
+  let params = [];
+  db.all(sql, params, (err: any, rows: any) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows,
+    });
+  });
+  // close the database connection
+  db.close();
 });
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+app.get('/duas/duacategory', (req: Request, res: Response) => {
+  let sql = 'select * from category';
+  let params = [];
+  db.all(sql, params, (err: any, row: any) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row,
+    });
+  });
+  // close the database connection
+  db.close();
+});
+
+// root routes
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello Next' });
+});
+
+// app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
 export default app;
