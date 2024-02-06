1b012eb433297a0e60b2fae6b7090936f63b75c9
Md. Sabbir Hossain <shr472978@gmail.com>
2024-02-06 23:00:38 +0600

    newtrial

--- server/index.ts
+++ server/index.ts
@@ -17,43 +17,43 @@ let db = new sqlite3.Database(DBSOURCE, sqlite3.OPEN_READWRITE, (err: any) => {
 });
 // get post routes
 
-app.get('/duas', (req: Request, res: Response) => {
-  let sql = 'select * from dua';
-  let params = [];
-  db.all(sql, params, (err: any, rows: any) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows,
-    });
-  });
-  // close the database connection
-  db.close();
-});
+// app.get('/duas', (req: Request, res: Response) => {
+//   let sql = 'select * from dua';
+//   let params = [];
+//   db.all(sql, params, (err: any, rows: any) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows,
+//     });
+//   });
+//   // close the database connection
+//   db.close();
+// });
 
-app.get('/duas/duacategory', (req: Request, res: Response) => {
-  let sql = 'select * from category';
-  let params = [];
-  db.all(sql, params, (err: any, row: any) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: row,
-    });
-  });
-  // close the database connection
-  db.close();
-});
+// app.get('/duas/duacategory', (req: Request, res: Response) => {
+//   let sql = 'select * from category';
+//   let params = [];
+//   db.all(sql, params, (err: any, row: any) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: row,
+//     });
+//   });
+//   // close the database connection
+//   db.close();
+// });
 
 // root routes
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello Next' });
+  res.status(200).json({ message: 'Hello Next - vercel back' });
 });
 
 // app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
